0025bf3002778bae70e65730d39c9c1d2ae925fd
blacklistzero <njjghtdrg@gmail.com>
2017-02-10 11:57:08 +0700

    Findpath image ok

--- app/src/main/java/blacx/ananboonmaut/bsruproject/SignUpActivity.java
+++ app/src/main/java/blacx/ananboonmaut/bsruproject/SignUpActivity.java
@@ -1,7 +1,14 @@
 package blacx.ananboonmaut.bsruproject;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +21,10 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageView imageView;
     private RadioGroup radioGroup;
     private Button button;
-    private String nameString, userString, passString;  // ประกาศตัวแปรใหม่
-
+    private String nameString, userString, passString,
+            pathImageString, nameImageString;  // ประกาศตัวแปรใหม่
+    private Uri uri;
+    private boolean aboolean = true;
 
 
     @Override
@@ -31,7 +40,79 @@ public class SignUpActivity extends AppCompatActivity {
 
         buttonController();
 
+        // image Controller
+        imageController();
+
+
     }  // Main Method
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText7);
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        button = (Button) findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            aboolean = false;
+
+            uri = data.getData();
+            // setup image choose to imageview
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Find path of image Choose ต้องการเลือกที่ๆรูปอยู่
+            String[]strings = new String[]{MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                pathImageString = cursor.getString(index);
+
+            } else {
+                pathImageString = uri.getPath();
+            }
+            Log.d("10febV1", "pathImage ==>" + pathImageString);
+
+
+
+
+
+        }  //if
+
+
+    }  //onActivityResult
+
+    private void imageController() {
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกแอพดูภาพ"), 1); // ถ้าต้องการอัพหลายรูปก็ทำ 1 2 3 4 5
+
+
+
+
+            }  // onclick
+        });
+
+
+    }   //image controller
 
     private void buttonController() {
         button.setOnClickListener(new View.OnClickListener() {
@@ -48,23 +129,24 @@ public class SignUpActivity extends AppCompatActivity {
                     //True ==> Have Space
 
                     MyAlert myAlert = new MyAlert(SignUpActivity.this);
-                    myAlert.myDialog("มีช่องว่าง","กรุณากรอกให้ครบทุกช่อง");
+                    myAlert.myDialog("มีช่องว่าง", "กรุณากรอกให้ครบทุกช่อง");
+                } else if (aboolean) {
+                    //non chose image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this);
+                    myAlert.myDialog("ยังไม่เลือกรูปภาพ", "กรุณาเลือกรูปภาพ");
 
 
+                } else {
+                    //EvertThing Ok
                 }
+
 
             }  //onClick
         });
-    }  //buttonController
+    }     //buttonController
 
-    private void bindWidget() {
 
-        nameEditText = (EditText) findViewById(R.id.editText7);
-        userEditText = (EditText) findViewById(R.id.editText6);
-        passEditText = (EditText) findViewById(R.id.editText5);
-        imageView = (ImageView) findViewById(R.id.imageView2);
-        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
-        button = (Button) findViewById(R.id.button);
-    }  // bindWidget
+
+
 
 }    // Main Class
